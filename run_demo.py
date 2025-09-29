cceba214a2c82c48c1914500b34c7d599d3bbfc5
prestonhemmy <phemmy@ufl.edu>
2025-09-29 13:16:54 -0400

    feat: skeleton structure for demo to be included in README

---
+++ run_demo.py
@@ -0,0 +1,12 @@
+import argparse
+from src import predict_single_news, train_model, CLASS_NAMES
+
+def interactive_demo():
+    """TODO: Takes modes train (to train the model)
+        and interactive (to use the trained model)"""
+
+def main():
+    """TODO"""
+
+if __name__ == '__main__':
+    main()
